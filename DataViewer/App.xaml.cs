6c7cbb1e19261e3a74bc55a0fac73be0d16a041b
martin_holkovic <dev.virgil@gmail.com>
2025-04-15 20:32:19 +0200

    add view for displaying BooksLibrary class

--- DataViewer/App.xaml.cs
+++ DataViewer/App.xaml.cs
@@ -1,9 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Configuration;
-using System.Data;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace DataViewer
 {
@@ -12,6 +11,36 @@ namespace DataViewer
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            App.Current.DispatcherUnhandledException += Current_DispatcherUnhandledException;
+
+            object viewModel = null;
+            var mainWindow = new MainWindow { DataContext = viewModel };
+
+            mainWindow.Show();
+        }
+
+        private void Current_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            HandleException(e);
+            e.Handled = true;
+        }
+
+        private static void HandleException(DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(FormatException(e.Exception), "DispatcherUnhandledException", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private static string FormatException(Exception exception)
+        {
+            return string.Format(
+                "DispatcherUnhandledException: {0}: {1}\n\nStack trace:\n{2}",
+                exception.GetType().FullName,
+                exception.Message,
+                exception.StackTrace);
+        }
     }
 
 }
